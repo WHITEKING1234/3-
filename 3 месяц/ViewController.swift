ac5b50e7a5c28a7aa8911a8ac81b9c3be1fdb75f
bombag456 <bombag456@gmail.com>
2022-11-17 18:30:03 +0600

    Initial Commit

---
+++ 3 месяц/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3 месяц
+//
+//  Created by Mac on 17/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
